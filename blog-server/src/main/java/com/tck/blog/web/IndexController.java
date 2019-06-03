d7db8dc464f0eb0503f50536095819489a7b18aa
tck8888 <380054728@qq.com>
2019-06-03 23:37:52 +0800

    添加html页面

--- blog-server/src/main/java/com/tck/blog/web/IndexController.java
+++ blog-server/src/main/java/com/tck/blog/web/IndexController.java
@@ -14,14 +14,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id,@PathVariable String name) {
-//      //  int i= 9/0;
-//        String blog = null;
-//        if (blog==null){
-//            throw  new NotFoundException("博客不存在");
-//        }
-        System.out.println("----------------------------------");
+    @GetMapping("/")
+    public String index() {
         return "index";
     }
 }
